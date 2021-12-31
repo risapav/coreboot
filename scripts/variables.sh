9a4beb77417ac104fa35c44955f9f394061085c8
risapav <risapav@gmail.com>
2021-12-31 10:27:07 +0100

    Update variables.sh

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -4,19 +4,33 @@
 set -e
 echo "Entering variables.sh"
 ################################################################################
-## VARIABLES
+## VARIABLES - necessary update before 
 ################################################################################
 export DOCKER_CONTAINER_NAME="coreboot-sdk"
 
+## maiboard vendor
 export MAINBOARD="lenovo"
-export MODEL="t410"
-export STOCK_BIOS_ROM="stock_bios.bin"
 
+## mainboard model
+export MODEL="t410"
+
+## original bios to extract ME from
+export STOCK_BIOS_ROM="stock_bios.bin"
+export VBIOS_ROM="vbios.bin"
+
+## picture 
+export BOOTSPLASH="bootsplash.jpg"
+
+################################################################################
+## MODEL VARIABLES
+################################################################################
 # export COREBOOT_SDK_VERSION="2021-09-23_b0d87f753c"
 # export COREBOOT_SDK_VERSION="6065f616eb"
 export COREBOOT_SDK_VERSION="2021-04-06_7014f8258e"
 
-# docker tree
+################################################################################
+## docker tree
+################################################################################
 export DOCKER_ROOT_DIR="/home/sdk"
 export DOCKER_PROJECT_DIR="$DOCKER_ROOT_DIR/prj"
 export DOCKER_SCRIPT_DIR="$DOCKER_PROJECT_DIR/scripts"
@@ -27,7 +41,9 @@ export DOCKER_COREBOOT_BUILD_DIR="$DOCKER_APP_DIR/build"
 export DOCKER_COREBOOT_DIR="$DOCKER_COREBOOT_BUILD_DIR"
 export DOCKER_COREBOOT_CONFIG_DIR="$DOCKER_COREBOOT_DIR/configs"
 
-# project tree
+################################################################################
+## project tree
+################################################################################
 export PROJECT_ROOT_DIR="."
 export PROJECT_SCRIPT_DIR="$PROJECT_ROOT_DIR/scripts"
 export PROJECT_APP_DIR="$PROJECT_ROOT_DIR/$MODEL"
