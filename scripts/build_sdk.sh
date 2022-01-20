b673c607d96027775172b75257a3488999360d26
risapav <risapav@gmail.com>
2022-01-20 10:20:17 +0100

    Update build_sdk.sh

--- scripts/build_sdk.sh
+++ scripts/build_sdk.sh
@@ -4,8 +4,8 @@
 # part no.1
 # check for running docker
 if ! docker info > /dev/null 2>&1; then
-  echo "Docker is missing..."
-  echo "This script uses docker, and if it isn't running - please start docker and try again!"
+  e_error "Docker is missing..."
+  e_warning "This script uses docker, and if it isn't running - please start docker and try again!"
   exit 1
 fi
 
@@ -16,19 +16,19 @@ DOCKER_IMAGE_ID=$( docker images --format "{{.ID}}" --filter=reference=$DOCKER_C
 if [[ -z $DOCKER_IMAGE_ID ]]; then
   # Coreboot
   if [[ ! -d $WORKER_DIR ]]; then
-    printf "Downloading Coreboot\n"
+    e_note "Downloading Coreboot\n"
     git clone --recursive $COREBOOT_SDK_REPOSITORY $WORKER_DIR
   else
-    printf "Coreboot repository is already present\n"
+    e_success "Coreboot repository is already present\n"
   fi
 
   # coreboot-sdk is necessary to create
   docker build $WORKER_DIR -t $DOCKER_CONTAINER_NAME --build-arg BUILD_DIR=$BUILD_DIR --build-arg COREBOOT_SDK_TAG=$COREBOOT_SDK_TAG --build-arg ARCH=$ARCH
   if [[ $? -ne 0  ]]; then
-    echo "Docker image $DOCKER_CONTAINER_NAME can not create..."
+    e_error "Docker image $DOCKER_CONTAINER_NAME can not create..."
     exit 1
   fi
-  echo "Docker image $DOCKER_CONTAINER_NAME is ready to use "
+  e_success "Docker image $DOCKER_CONTAINER_NAME is ready to use "
 fi
 
 # part no.3 todo reusing docker proces
@@ -36,8 +36,8 @@ fi
 DOCKER_CONTAINER_ID=$( docker ps --format "{{.ID}}" --filter ancestor=$DOCKER_CONTAINER_NAME )
 
 if [ "$( docker container inspect -f '{{.State.Status}}' $DOCKER_CONTAINER_NAME )" == "running" ]; then 
-  echo "coreboot_sdk is not running..."
-  echo $DOCKER_CONTAINER_ID
+  e_error "coreboot_sdk is not running..."
+  e_wrning $DOCKER_CONTAINER_ID
 fi
 
 exit 0
